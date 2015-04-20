6681bf74142ea3f6814b992d93c4632f19df9fca
vjancic <vedran.jancic@gmail.com>
2015-04-21 01:57:11 +0200

    Route controller

---
+++ client/scripts/controls/router.js
@@ -0,0 +1,37 @@
+/*jslint nomen: true, white: true */
+
+(function (define, angular, document) {
+    'use strict';
+
+    define(['ctl/ads'], function () {
+        var app = angular.module('app');
+        app.config(function ($stateProvider, $urlRouterProvider) {
+
+            $urlRouterProvider.otherwise('/ads');
+
+            $stateProvider.state('ads', {
+                url: '/ads',
+                templateUrl: 'templates/main.html',
+                controller: 'adsController'
+            });
+
+            $stateProvider.state('view', {
+                url: '/ad/:id',
+                templateUrl: 'templates/view.html',
+                controller: function ($log) {
+                    $log.log('Show ad!');
+                }
+            });
+
+			$stateProvider.state('search', {
+                url: '/ads/:action/:id',
+                templateUrl: 'templates/main.html',
+                controller: 'adsController'
+            });
+        });
+
+        angular.element(document).ready(function () {
+            angular.bootstrap(document, ['app']);
+        });
+    });
+}(this.define, this.angular, this.document));
