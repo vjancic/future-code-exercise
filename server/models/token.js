a5d62b7eee31c7aa30727fca4410aa7c8632b098
vjancic <vedran.jancic@gmail.com>
2015-05-01 00:09:34 +0200

    Token model

---
+++ server/models/token.js
@@ -0,0 +1,17 @@
+/*jslint node: true, nomen: true*/
+
+var mongoose = require('mongoose'),
+    schema =  new mongoose.Schema({
+        token: {
+            type: String,
+            unique: true,
+            index: true
+        },
+        userId: {
+            type: String,
+            unique: true,
+            index: true
+        }
+    });
+
+module.exports = mongoose.model('Token', schema);
