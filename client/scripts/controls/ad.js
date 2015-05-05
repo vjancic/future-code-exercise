6db95ad1355d46ef24a6979b4ea41668d0385618
vjancic <vedran.jancic@gmail.com>
2015-05-05 18:52:44 +0200

    Ad now shows activity, and can leave a comment or lock the ad

--- client/scripts/controls/ad.js
+++ client/scripts/controls/ad.js
@@ -5,18 +5,28 @@
 
     define(function () {
     	var app = angular.module('app'),
-            address,
+            getAddress,
+            postAddress,
             responsePromise;
 
     	app.controller('adController',
-            ['$stateParams', '$scope', '$http', '$log',
-            function ($stateParams, $scope, $http, $log) {
-                address = "/ad/" + $stateParams.id;
+            ['$stateParams', '$scope', '$http', '$log', 'auth',
+            function ($stateParams, $scope, $http, $log, auth) {
+                getAddress = "/ad/" + $stateParams.id;
+                postAddress = "/activity/" + $stateParams.id + "/" + auth.getInfo();
                 $scope.data = {};
-                $scope.data.ads = [];
 
                 function ajaxSuccess(data) {
-                    $scope.data.ads = Array.prototype.slice.call(data);
+                    var isLoggedIn = auth.isLoggedIn(),
+                        isNotExpired;
+
+                    if (data instanceof Array && data.length) {
+                        $scope.data.ad = angular.copy(data[0]);
+
+                        isNotExpired = (data[0].expiry === 0 || data[0].expiry > new Date().getTime());
+
+                        $scope.data.canLock = isLoggedIn && isNotExpired;
+                    }
                 }
 
                 function ajaxError(data, status) {
@@ -24,9 +34,23 @@
                     $log.error(data);
                 }
 
-                responsePromise = $http.get(address);
-                responsePromise.success(ajaxSuccess);
-                responsePromise.error(ajaxError);
+				function getAd() {
+					responsePromise = $http.get(getAddress);
+					responsePromise.success(ajaxSuccess);
+					responsePromise.error(ajaxError);
+				}
+
+				$scope.data.submit = function (type) {
+                    responsePromise = $http.post(postAddress,
+                    {
+                        type: type,
+                        comment: $scope.data.comment
+                    });
+                    responsePromise.success(ajaxSuccess);
+					responsePromise.error(ajaxError);
+				};
+
+				getAd();
         }]);
     });
 }(this.define, this.angular));
